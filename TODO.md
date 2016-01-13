f8eca603f482224c1617aad96030fa9a00ee027c
brianhouse <brian.house@gmail.com>
2016-01-12 19:17:47 -0500

    multiple sensors

---
+++ TODO.md
@@ -0,0 +1,9 @@
+- proper scaling
+- gravity normalization
+- x, y, z appropriate
+- rssi meters and labels
+
+
+green x
+blue y
+red z
--- main.py
+++ main.py
@@ -7,26 +7,32 @@ from housepy import config, log, util, animation
 from housepy.xbee import XBee
 from mongo import db
 
-samples = deque()
+sensor_data = {}
+sensor_rssi = {}
 sessions = deque()
 current_session = None
+
+RANGE = 1023
 
 def message_handler(response):
     # log.info(response)
     try:
-        global samples
         # print(response['sensor'], response['samples'], response['rssi'])
         t = util.timestamp(ms=True)
+        sensor = response['sensor']
+        sample = response['samples']
+        rssi = response['rssi']
         if current_session is not None:
-            data = {'t': t, 'sensor': response['sensor'], 'samples': response['samples'], 'rssi': response['rssi'], 'session': str(current_session)}
-            print(json.dumps(data, indent=4))
+            data = {'t': t, 'sensor': sensor, 'sample': sample, 'rssi': rssi, 'session': str(current_session)}
+            # print(json.dumps(data, indent=4))
             db.branches.insert(data)
-        data = [sample / 1023 for sample in response['samples']]
-        samples.appendleft((t, data))
-        sessions.appendleft(current_session)
-        if len(samples) == 1000:
-            samples.pop()
-            sessions.pop()
+        if sensor not in sensor_data:
+            sensor_data[sensor] = deque()
+            sensor_rssi[sensor] = None
+        sensor_data[sensor].appendleft((t, sample))
+        sensor_rssi[sensor] = rssi
+        if len(sensor_data[sensor]) == 1000:
+            sensor_data[sensor].pop()
     except Exception as e:
         log.error(log.exc(e))
 
@@ -42,21 +48,21 @@ def stop_session():
     current_session = None
 
 def draw():
-    if len(samples):
-        x_points = [(s / ctx.width, sample[1][0]) for (s, sample) in enumerate(list(samples))]
-        y_points = [(s / ctx.width, sample[1][1]) for (s, sample) in enumerate(list(samples))]
-        z_points = [(s / ctx.width, sample[1][2]) for (s, sample) in enumerate(list(samples))]
-        ctx.lines(x_points, color=(1., 0., 0., 1.), thickness=0.5)    
-        ctx.lines(y_points, color=(0., 1., 0., 1.), thickness=0.5)    
-        ctx.lines(z_points, color=(0., 0., 1., 1.), thickness=0.5) 
-    i = 0
-    while i < len(sessions) and sessions[i] is None:
-        i += 1
-    if i != len(sessions):
-        j = i
-        while j < len(sessions) and sessions[j] is not None:
-            j += 1
-        ctx.line(i / ctx.width, 0.98, j / ctx.width, 0.98, color=(1., 0., 0., .25), thickness=50.0)
+    for sensor in sensor_data:
+        samples = sensor_data[sensor]
+        # print(samples)
+        if len(samples):
+            ctx.plot([sample[1][0] / 1023 for sample in samples], color=(1., 0., 0., 1.))
+            ctx.plot([sample[1][1] / 1023 for sample in samples], color=(0., 1., 0., 1.))
+            ctx.plot([sample[1][2] / 1023 for sample in samples], color=(0., 0., 1., 1.))
+    # i = 0
+    # while i < len(sessions) and sessions[i] is None:
+    #     i += 1
+    # if i != len(sessions):
+    #     j = i
+    #     while j < len(sessions) and sessions[j] is not None:
+    #         j += 1
+    #     ctx.line(i / ctx.width, 0.98, j / ctx.width, 0.98, color=(1., 0., 0., .25), thickness=50.0)
 
 def on_mouse_press(data):
     # x, y, button, modifers = data
